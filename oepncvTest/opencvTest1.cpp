f43189c20e4356094e041200377541af9688d50f
Juhyeon AN <ajh9789@gachon.ac.kr>
2025-04-21 14:39:51 +0900

    이미지에 흑백처리하고 출력

--- oepncvTest/opencvTest1.cpp
+++ oepncvTest/opencvTest1.cpp
@@ -1,4 +1,4 @@
-﻿
+﻿#include <opencv2/imgproc.hpp> 
 #include <opencv2/highgui.hpp>
 #include <iostream> 
 using namespace cv;
@@ -10,8 +10,19 @@ if (img.empty()) { //이미지 비어있으면
 	cerr << "Error: Could not load image!" << endl; //예외 처리
 	return -1;//종료
 }
-imshow("Lenna", img); //이미지 출력
-waitKey(0);//키 입력 대기
+// 1. 그레이스케일 변환
+cv::Mat gray;
+cv::cvtColor(img, gray, cv::COLOR_BGR2GRAY);
+
+// 2. 컬러맵 적용 (흑백)
+cv::Mat xray;
+cv::applyColorMap(gray, xray, cv::COLORMAP_BONE);
+
+// 3. 이미지 출력
+cv::imshow("Original", img);
+cv::imshow("X-ray Style", xray);
+cv::waitKey(0);
+
 return 0;
 }
 
